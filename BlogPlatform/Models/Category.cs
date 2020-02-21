cb3e14c2c929ee2d81756872a61ea8acd7c3287e
Tatyana Bailey <btatyana45@gmail.com>
2020-02-21 18:05:24 -0500

    created Models, properties and constructors within

---
+++ BlogPlatform/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPlatform.Models
+{
+    public class Category
+    {
+        public string Name { get; set; }
+
+
+        public Category()
+        {
+        }
+
+        public Category(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ BlogPlatform/Models/Post.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPlatform.Models
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+
+
+        public Post()
+        {
+        }
+
+        public Post(string title, string body, string author, DateTime publishDate)
+        {
+            Title = title;
+            Body = body;
+            Author = author;
+            PublishDate = publishDate;
+        }
+    }
+
+
+
+}
