55bc50bf6de8db7112466af6e793ad32255d4a92
Tatyana Bailey <btatyana45@gmail.com>
2020-02-21 16:43:11 -0500

    configured startup class for MVC

---
+++ BlogPlatform.Tests/BlogPlatformTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BlogPlatform.Tests
+{
+    public class BlogPlatformTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
