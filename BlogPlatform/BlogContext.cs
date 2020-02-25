146e3380050cbdc60015b8bc2c6d1ee90fb48a76
anmesk <an_mesk@live.com>
2020-02-25 18:38:16 -0500

    Added seed data to BlogContext

--- BlogPlatform/BlogContext.cs
+++ BlogPlatform/BlogContext.cs
@@ -22,6 +22,58 @@ namespace BlogPlatform
 
             base.OnConfiguring(optionsBuilder);
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasData(
+                new Post()
+                {
+                    Id = 001,
+                    Title = "Bike Riding",
+                    Body = "This is my bike riding story.",
+                    Author = "Nabil",
+                    PublishDate = DateTime.Now
+                },
+
+                new Post()
+                {
+                    Id = 002,
+                    Title = "Photography",
+                    Body = "This is my photography story.",
+                    Author = "Tatyana",
+                    PublishDate = DateTime.Now
+                },
+
+                new Post()
+                {
+                    Id = 003,
+                    Title = "Blogging",
+                    Body = "This is a blogger's blog about blogging.",
+                    Author = "Blogger",
+                    PublishDate = DateTime.Now
+                },
+
+                new Post()
+                {
+                    Id = 004,
+                    Title = "Computer Hardware",
+                    Body = "This is a computer hardware blog post.",
+                    Author = "Stefan",
+                    PublishDate = DateTime.Now
+                });
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category()
+                {
+                });
+
+            modelBuilder.Entity<Tag>().HasData(
+                new Tag()
+                {
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
     
 }
