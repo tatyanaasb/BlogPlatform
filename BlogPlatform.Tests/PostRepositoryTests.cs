95d3be68353252395bc6ffe6fb484637c7c4f3eb
anmesk <an_mesk@live.com>
2020-02-25 18:01:59 -0500

    Carried out first migration, updated PostRepository

--- BlogPlatform.Tests/PostRepositoryTests.cs
+++ BlogPlatform.Tests/PostRepositoryTests.cs
@@ -7,8 +7,24 @@ using BlogPlatform.Repositories;
 
 namespace BlogPlatform.Tests
 {
-    public class PostRepositoryTests
+    public class PostRepositoryTests : IDisposable
     {
+        private BlogContext db;
+        private PostRepository underTest;
+
+        public PostRepositoryTests()
+        {
+            db = new BlogContext();
+            db.Database.BeginTransaction();
+
+            underTest = new PostRepository(db);
+        }
+
+        public void Dispose()
+        {
+            db.Database.RollbackTransaction();
+        }
+        
         [Fact]
         public void Count_Starts_At_Zero()
         {
@@ -18,6 +34,17 @@ namespace BlogPlatform.Tests
             var count = underTest.Count();
 
             Assert.Equal(0, count);
+        }
+        [Fact]
+        public void Create_Increases_Count()
+        {
+            var db = new BlogContext();
+            var underTest = new PostRepository(db);
+
+            underTest.Create(new Post() { Title = "Backpack" });
+
+            var count = underTest.Count();
+            Assert.Equal(1, count);
         }
     }
 }
--- BlogPlatform/Repositories/PostRepository.cs
+++ BlogPlatform/Repositories/PostRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlogPlatform.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,9 +13,20 @@ namespace BlogPlatform.Repositories
         {
             this.db = db;
         }
+
+        public Post GetById(int id)
+        {
+            return db.Posts.Single(p => p.Id == id);
+        }
         public int Count()
         {
             return db.Posts.Count();
+        }
+
+        public void Create(Post post)
+        {
+            db.Posts.Add(post);
+            db.SaveChanges();
         }
     }
 }
