75b82af2db2f4e8d7a9f926f2a57206dbee42222
Tatyana Bailey <btatyana45@gmail.com>
2020-02-26 16:46:23 -0500

    Added data of category and tag to our blogContext db

--- BlogPlatform/BlogContext.cs
+++ BlogPlatform/BlogContext.cs
@@ -29,7 +29,7 @@ namespace BlogPlatform
                 new Post()
                 {
                     Id = 001,
-                    Title = "Bike Riding",
+                    Title = "My Bike Riding Hobby",
                     Body = "This is my bike riding story.",
                     Author = "Nabil",
                     PublishDate = DateTime.Now
@@ -38,7 +38,7 @@ namespace BlogPlatform
                 new Post()
                 {
                     Id = 002,
-                    Title = "Photography",
+                    Title = "My Photography Hobby",
                     Body = "This is my photography story.",
                     Author = "Tatyana",
                     PublishDate = DateTime.Now
@@ -47,7 +47,7 @@ namespace BlogPlatform
                 new Post()
                 {
                     Id = 003,
-                    Title = "Blogging",
+                    Title = "My Blogging Hobby",
                     Body = "This is a blogger's blog about blogging.",
                     Author = "Blogger",
                     PublishDate = DateTime.Now
@@ -56,20 +56,100 @@ namespace BlogPlatform
                 new Post()
                 {
                     Id = 004,
-                    Title = "Computer Hardware",
-                    Body = "This is a computer hardware blog post.",
-                    Author = "Stefan",
+                    Title = "All About Movies",
+                    Body = "This is a blog about movies",
+                    Author = "Jeremy",
                     PublishDate = DateTime.Now
                 });
 
             modelBuilder.Entity<Category>().HasData(
                 new Category()
                 {
+                    Id = 01,
+                    Name = "Bike Riding"
+                },
+
+                new Category()
+                {
+                    Id = 02,
+                    Name = "Photography"
+                },
+
+                new Category()
+                {
+                    Id = 03,
+                    Name = "Blogging"
+                },
+
+                new Category()
+                {
+                    Id = 04,
+                    Name = "Movies"
                 });
 
             modelBuilder.Entity<Tag>().HasData(
                 new Tag()
                 {
+                    Id = 101,
+                    Name = "mountainbiking"
+                },
+
+                new Tag()
+                {
+                    Id = 102,
+                    Name = "fitness"
+                },
+
+                new Tag()
+                {
+                    Id = 103,
+                    Name = "adventure"
+                },
+
+                new Tag()
+                {
+                    Id = 201,
+                    Name = "photooftheday"
+                },
+                new Tag()
+                {
+                    Id = 202,
+                    Name = "naturephotography"
+                },
+                new Tag()
+                {
+                    Id = 203,
+                    Name = "travelphotography"
+                },
+                new Tag()
+                {
+                    Id = 301,
+                    Name = "lifestyleblogger"
+                },
+                new Tag()
+                {
+                    Id = 302,
+                    Name = "socialmedia"
+                },
+                new Tag()
+                {
+                    Id = 303,
+                    Name = "digitalmarketing"
+                },
+                new Tag()
+                {
+                    Id = 401,
+                    Name = "comedy"
+                },
+                new Tag()
+                {
+                    Id = 402,
+                    Name = "moviebuff"
+                },
+                new Tag()
+                {
+                    Id = 403,
+                    Name = "filmmaking"
                 });
 
             base.OnModelCreating(modelBuilder);
