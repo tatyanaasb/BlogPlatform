54defa80495ecbcd0e1fb2e9937c6e442c58228d
Tatyana Bailey <btatyana45@gmail.com>
2020-02-28 15:20:33 -0500

    categoryController methods and actions

--- BlogPlatform/Controllers/CategoryController.cs
+++ BlogPlatform/Controllers/CategoryController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BlogPlatform.Models;
+using BlogPlatform.Repositories;
+
 
 namespace BlogPlatform.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        IRepository<Category> categoryRepo;
+
+        public CategoryController(IRepository<Category> categoryRepo)
         {
-            return View();
+            this.categoryRepo = categoryRepo;
         }
+
+        public ViewResult Index()
+        {
+            var model = categoryRepo.GetAll();
+
+            return View(model);
+        }
+
+        public ViewResult Details(int id)
+        {
+            Category model = categoryRepo.GetById(id);
+
+            return View(model);
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
     }
 }
