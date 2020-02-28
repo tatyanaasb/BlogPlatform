f06c5abe19cd412e06d1eacdc314c77f8f6fc48f
anmesk <an_mesk@live.com>
2020-02-28 11:11:36 -0500

    Created PostController and connected it to IRepository

---
+++ BlogPlatform/Controllers/PostController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BlogPlatform.Models;
+using BlogPlatform.Repositories;
+
+namespace BlogPlatform.Controllers
+{
+    public class PostController : Controller
+    {
+        IRepository<Post> postRepo;
+
+        public PostController(IRepository<Post> postRepo)
+        {
+            this.postRepo = postRepo;
+        }
+        
+        public ViewResult Index(string title)
+        {
+            var model = new Post();
+            model.Title = title;
+            return View(model);
+        }
+    }
+}
