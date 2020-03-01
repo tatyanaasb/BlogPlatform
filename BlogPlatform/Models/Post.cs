8c10446e604ced810d34b530f9fc03fc28a4b9d3
anmesk <an_mesk@live.com>
2020-02-29 21:24:15 -0500

    Added error messages to display if user input is empty

--- BlogPlatform/Models/Post.cs
+++ BlogPlatform/Models/Post.cs
@@ -9,13 +9,15 @@ namespace BlogPlatform.Models
     public class Post
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "You must enter an Author name.")]
         public string Author { get; set; }
+        [Required(ErrorMessage = "You must enter a Post Title.")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "You must enter Post Content.")]
         public string Body { get; set; }
         public DateTime PublishDate { get; set; }
 
         // Attempting 1-Many with Category:Post
-        //[Display(Name = "Select a Category: ")]
         public virtual Category Category { get; set; }
         public virtual int CategoryId { get; set; }
 
